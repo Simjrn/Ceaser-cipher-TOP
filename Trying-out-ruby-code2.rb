926e8d97f7cf96e73071b5f5e4c9efb4a48a3417
Simjrn <simonjenni374@gmail.com>
2025-07-31 09:01:00 +0100

    Update Trying-out-ruby-code2.rb

--- Trying-out-ruby-code2.rb
+++ Trying-out-ruby-code2.rb
@@ -4,7 +4,7 @@ puts "Write your word below"
 str = gets.chomp
 wordtonum = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16}
 converted1 = {}
-str.split("")!
+str = str.split("")!
 def conversion1(string)
     for letter in string do
         converted1.push[wordtonum[letter]]
@@ -18,6 +18,6 @@ def conversion2(convert)
         finish.push[numtoword[num]]
     end
 end
-conversion1(str)!
+conversion1(str)
 puts conversion2(converted1)
 puts "That's that done!"
