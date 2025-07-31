b0675af80d7105eedb34853b37dd9b4c372d6d7c
Simjrn <simonjenni374@gmail.com>
2025-07-31 08:52:28 +0100

    Update Trying-out-ruby-code2.rb
    Added more items to a hash

--- Trying-out-ruby-code2.rb
+++ Trying-out-ruby-code2.rb
@@ -2,7 +2,7 @@ puts "Write your number below"
 num = gets.chomp
 puts "Write your word below"
 str = gets.chomp
-wordtonum = {"a" => 1, "b" => 2, "c" => 3}
+wordtonum = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16}
 converted1 = {}
 str.split("")!
 def conversion1(string)
