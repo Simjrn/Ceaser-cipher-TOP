1d5c61586ce63bc89ae3cefb496366bcca258e64
Simjrn <simonjenni374@gmail.com>
2025-07-31 20:05:31 +0100

    Update Ceaser-cipher-code-2.0(working version).rb

--- Ceaser-cipher-code-2.0(working version).rb
+++ Ceaser-cipher-code-2.0(working version).rb
@@ -4,18 +4,18 @@ def ceaser_cipher(message, change) # message is what we're encoding, change is h
   wordtonum = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26}
   numtoword = {1 => "a", 2 => "b", 3 => "c", 4 => "d", 5 => "e", 6 => "f", 7 => "g", 8 => "h", 9 => "i", 10 => "j", 11 => "k", 12 => "l", 13 => "m", 14 => "n", 15 => "o", 16 => "p", 17 => "q", 18 => "r", 19 => "s", 20 => "t", 21 => "u", 22 => "v", 23 => "w", 24 => "x", 25 => "y", 26 => "z"}
   message.each_char do |char| # Repeat for every character in the message
-    if char = " "
+    if char == " "
       result += char
     else
       char = wordtonum[char] # use the first hash to change all the letters into numbers
-      char char += change # add the amount we're changing by onto the numbers
+      char += change # add the amount we're changing by onto the numbers
       if char > 26 # make sure our number is under 26, if not we'll wrap around
-        char = char -= 26
+        char -= 26
       end
       char = numtoword[char] # change the numbers into letters 
+      result += char
     end
   end
-  result += char
   return char
 end
 ceaser_cipher("Pez", 4)         
